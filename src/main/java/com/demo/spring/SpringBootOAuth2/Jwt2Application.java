b1ecd9f36784280b2a907cbaa1df51f2b3725fe4
Jayyy <korrakote.j@gmail.com>
2018-06-12 20:27:45 +0700

    init

---
+++ src/main/java/com/demo/spring/SpringBootOAuth2/Jwt2Application.java
@@ -0,0 +1,12 @@
+package com.demo.spring.SpringBootOAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwt2Application.class, args);
+	}
+}
